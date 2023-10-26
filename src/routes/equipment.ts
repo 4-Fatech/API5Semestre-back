e66848b04acb86b675507b744d931d1efa0b8fff
dsslleagion <dionisio.leao@fatec.sp.gov.br>
2023-10-25 22:44:41 -0300

    teste

--- src/routes/equipment.ts
+++ src/routes/equipment.ts
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import { EquipmentController } from "../controllers";
+import { authorization } from "../middlewares";
 
 const routes = Router();
 
-routes.post("/create", EquipmentController.create); 
-routes.get("/list", EquipmentController.list);
-routes.get("/get/:id", EquipmentController.one);
-routes.post("/delete", EquipmentController.delete);
-routes.patch("/update", EquipmentController.update);
+routes.post("/create",authorization, EquipmentController.create); 
+routes.get("/list",authorization, EquipmentController.list);
+routes.get("/get/:id",authorization, EquipmentController.one);
+routes.post("/delete",authorization, EquipmentController.delete);
+routes.patch("/update",authorization, EquipmentController.update);
 
 
 export default routes;
