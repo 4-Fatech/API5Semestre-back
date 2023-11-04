4c76b84392ca33844cc2ef55f4fa0bc92f0c2784
dsslleagion <dionisio.leao@fatec.sp.gov.br>
2023-11-04 18:35:19 -0300

    authorization

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,18 +1,19 @@
 import { Router } from "express";
 import { UserController } from "../controllers";
+import { authorization } from "../middlewares";
 
 const routes = Router();
 
-routes.post("/create", UserController.create); 
-routes.get("/list", UserController.list);
-routes.delete("/delete", UserController.delete);
-routes.patch("/update/:id", UserController.update);
-routes.get("/list/:id", UserController.one);
-routes.put("/notEmail", UserController.notEmail);
-routes.put("/notSms", UserController.notSms);
-routes.put("/valNot", UserController.valNot);
+routes.post("/create",authorization , UserController.create); 
+routes.get("/list",authorization , UserController.list);
+routes.delete("/delete",authorization , UserController.delete);
+routes.patch("/update/:id",authorization , UserController.update);
+routes.get("/list/:id",authorization , UserController.one);
+routes.put("/notEmail",authorization , UserController.notEmail);
+routes.put("/notSms",authorization , UserController.notSms);
+routes.put("/valNot",authorization , UserController.valNot);
 routes.put("/atualizarSenha", UserController.atualizarSenha);
-routes.patch("/updatePerfil/:id", UserController.updatePerfil);
+routes.patch("/updatePerfil/:id",authorization , UserController.updatePerfil);
 routes.get("/keepUserLoggedIn", UserController.keepUserLoggedIn);
 
 export default routes;
